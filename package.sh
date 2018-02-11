97ab9fb4c94adc9b57e33e6b699d64c79a425032
Reid Zeng <wenchao@yit.com>
2018-02-11 14:19:37 +0800

    Add package file

---
+++ package.sh
@@ -0,0 +1,24 @@
+mvn clean
+mvn package
+
+# 拷贝文件到目标目录
+rm -rdf ./target/release
+mkdir ./target/release
+cp ./target/screenbroadcast*.jar ./target/release/ScreenbBroadcast.jar
+cp -R ./web ./target/release/
+
+# mac app打包: http://centerkey.com/mac/java/
+jdk=$(/usr/libexec/java_home)
+$jdk/bin/javapackager \
+   -deploy \
+   -native image \
+   -name ScreenBroadcast \
+   -BappVersion=0.1.0 \
+   -Bicon=app.icns \
+   -srcdir . \
+   -srcfiles target/release/ScreenbBroadcast.jar \
+   -appclass com.zwc.screenbroadcast.Main \
+   -outdir target/release/result \
+   -outfile ScreenBroadcast \
+   -nosign \
+   -v
--- src/main/java/com/zwc/screenbroadcast/Main.java
+++ src/main/java/com/zwc/screenbroadcast/Main.java
@@ -1,14 +1,18 @@
 package com.zwc.screenbroadcast;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * 主入口函数
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.out.println("Hello");
 
+        Files.write(Paths.get("/Users/reid.zeng/Desktop/yit/gitlab/ScreenBroadcast/target/release/test.txt"), "Hello world".getBytes());
     }
 
 }
